a0a518c8587a6639cc268ec458fed0cc7a78c8ae
jiawangyu <jiawang.yu@gmail.com>
2018-10-29 15:50:41 +0800

    Inherited TrinityPlugin

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -18,6 +18,7 @@
 */
 package org.apache.cordova.camera;
 
+import org.elastos.trinity.dapprt.TrinityPlugin;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -71,7 +72,7 @@ import android.content.pm.PackageManager.NameNotFoundException;
  * and returns the captured image.  When the camera view is closed, the screen displayed before
  * the camera view was shown is redisplayed.
  */
-public class CameraLauncher extends CordovaPlugin implements MediaScannerConnectionClient {
+public class CameraLauncher extends TrinityPlugin implements MediaScannerConnectionClient {
 
     private static final int DATA_URL = 0;              // Return base64 encoded string
     private static final int FILE_URI = 1;              // Return file uri (content://media/external/images/media/2 for Android)
